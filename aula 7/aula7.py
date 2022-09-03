79ac9e0a5c933354a505e091619d1c8a91bf2217
ruby --version <paulinn.rs@gmail.com>
2022-09-03 11:45:05 -0300

    atualizando curso

---
+++ aula 7/aula7.py
@@ -0,0 +1,16 @@
+nome = 'Paulo Roberto'
+idade = 37  #int
+altura = 1.71   #float
+e_maior = idade > 18    #bool
+peso = 70
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'anos de e seu imc é', imc)
+print(f'{nome} tem {idade} anos de e seu imc é {imc:.2f}')
+print('{} tem {} anos de , {} de altura, peso {} e seu imc é {:.2f}'.format(nome, idade, altura, peso, imc))
+"""
+podemos mudar as ordens das variaveis e repertir
+"""
+print('{0} tem {1} anos e seu imc é {2:.2f}'.format(nome, idade, imc))
+print('eu tenho {i} anos me chamo {n} e meu imc é {im:.2f}, com {i} anos estou muito bem.'.format(n=nome, i=idade, im=imc))
+
---
+++ aula 9/aula9.py
@@ -0,0 +1,8 @@
+'''
+Entrada de dados
+'''
+
+campeao = input("quem vai ganhar a libertatores 2022? ")
+titulos = input("com quantas libertadores o flamengo vai ficar? ")
+print()
+print(f'em 2022 o {campeao} vai ficar com {titulos} libertadores')
